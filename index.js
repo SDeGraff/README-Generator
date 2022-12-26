15192864a2b44d2da11257f674234c8330f18ec8
SDeGraff <114950127+SDeGraff@users.noreply.github.com>
2022-12-26 15:13:17 -0600

    questions to index

--- index.js
+++ index.js
@@ -4,13 +4,63 @@ const inquirer = require("inquirer");
 const generateMarkdown = require(./utils/generateMarkdown)
 
 // TODO: Create an array of questions for user input
+
 const questions = [
     {
         type: "input",
         name: "title",
-        message: "what is the title of your project"
-
-    }
+        message: "what is the title of your project?",
+        default: "README.md File Generator",
+    },
+    {
+        type: "input",
+        name: "authorGithub",
+        message: "what is your github username?",
+    },
+    {
+        type: "input",
+        name: "repoName",
+        message: "what is the name of the github repository?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of the project. ",
+    },
+    {
+        type: "checkbox",
+        name: "sections",
+        message: "Select which sections to add",
+        choices: [
+            new inquirer.Separator("= Choose a Sections ="),
+            {name: "installation"},
+            {name: "screenshots"},
+            {name: "tool list"},
+            {name: "license"}
+        ]
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Input any installation commands separated by a comma. ",
+        when: (data) => (data.sections.indexOf("installation") >= 0)
+    },
+    {
+        type: "input",
+        name: "authorName",
+        message: "What is the name of the Author? ",
+    },
+    {
+        type: "input",
+        name: "authorLinkedIn",
+        message: "What is your LinkedIn Username? ",
+    },
+    {
+        type: "input",
+        name: "screenshot",
+        message: "Enter screenshot file name:",
+        when: (data) => (data.sections.indexOf("screenshots") >= 0)
+    },
     { 
         type: "List"
         name: "License"
