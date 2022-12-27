a561e8d71695e8d12cb24e9f0febc824dfe1ed83
SDeGraff <114950127+SDeGraff@users.noreply.github.com>
2022-12-27 11:30:28 -0600

    license links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -16,16 +16,24 @@ function renderLicenseBadge(license) {
   // TODO: Create a function that returns the license link
   // If there is no license, return an empty string
   function renderLicenseLink(license) {
-    
+    if (license === "MIT") {
+      licenseLink === "(https://opensource.org/licenses/MIT)"
+    } else if (license === "Apache 2.0") {
+      licenseLink === "(https://opensource.org/licenses/Apache-2.0)"
+    } else if ( license === "Mozilla") {
+      licenseLink === "(https://opensource.org/licenses/MPL-2.0)"
+    } else (license === "none") {
+      licenseLink = "";
     }
-  }
+    return licenseLink;
+   }
+  
   
   // TODO: Create a function that returns the license section of README
   // If there is no license, return an empty string
   function renderLicenseSection(license) {
     if(license !== "none") {
       return `## license 
-      
       This application is under the ${license} license.`
     } return "";
   }
